8d4a667b28658edf249bb7df1dca79106ccd23cd
xhmiao <3466187547@qq.com>
2019-09-22 16:49:29 +0800

    nothing new, confuse by sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,11 +2,15 @@ const navConf = require('../config/navConf.js');
 const sidebarConf = require('../config/sidebarConf/index.js');
 
 module.exports = {
-    title: "Welcome to xhMiao's test documents page",
-    themeConfig:{
-        nav: navConf,
-        description:'test vuepress documents',
-        sidebar: sidebarConf,
-        lastupdated: '上次更新',
+    title: "IAES docs test",
+    description: 'test vuepress documents',
+    themeConfig: {
+        // nav: navConf,
+        sidebar: [
+            '/',
+            '/page-a',
+            ['/page-b', 'Explicit link text']
+          ]
+        // lastupdated: '上次更新',
     }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,2 +1,10 @@
-hello, welcome to xhmiao's website.  
-Good luck!
+---
+home: true
+actionText: 快速放弃
+actionLink: /guide/
+---
+## nodejs版本: v10.16.0
+## webpack版本: 4.38.0
+## <font color='red'> 可以使用npm安装vuepress，测试通过</font>
+***
+# Good Luck!
